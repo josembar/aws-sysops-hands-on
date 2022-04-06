1339d211fc3ff6f47769b803ed0650e1b6764093
Jose Barrantes <jose.barrantes@hotmail.es>
2022-04-06 02:47:27 -0500

    Output public ip address

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -5,3 +5,7 @@ output "instance_arn" {
 output "instance_id" {
   value = module.ec2_instance.instance_id
 }
+
+output "instance_public_ip" {
+  value = module.ec2_instance.instance_public_ip
+}
--- modules/ec2_instance/outputs.tf
+++ modules/ec2_instance/outputs.tf
@@ -5,3 +5,7 @@ output "instance_arn" {
 output "instance_id" {
   value = aws_instance.instance.id
 }
+
+output "instance_public_ip" {
+  value = aws_instance.instance.public_ip
+}
