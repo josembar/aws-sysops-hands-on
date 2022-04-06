ab27bf78bb20a192f14ef2ba85db9c7107f0ae7b
Jose Barrantes <jose.barrantes@hotmail.es>
2022-04-06 03:06:23 -0500

    Key pair association

--- ec2/main.tf
+++ ec2/main.tf
@@ -8,13 +8,13 @@ resource "aws_key_pair" "key_pair_demo" {
 }
 
 # instance creation
-
 module "ec2_instance" {
   source                  = "../modules/ec2_instance"
   ami                     = data.aws_ami.amazon_linux_2.id
   instance_type           = local.instance.instance_type
   shutdown_behavior       = local.instance.shutdown_behaviors[1]
   disable_api_termination = false
+  key_name                = aws_key_pair.key_pair_demo.id
   tags = {
     Name = local.instance.name
   }
