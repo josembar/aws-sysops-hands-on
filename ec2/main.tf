d813f1dc011f5bb0e8cb29060547a0879d0561ae
Jose Barrantes <jose.barrantes@hotmail.es>
2022-04-06 01:36:50 -0500

    EC2 creation

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,10 +1,11 @@
 # instance creation
 
 module "ec2_instance" {
-  source            = "./modules/ec2_instance"
-  ami               = data.aws_ami.amazon_linux_2.id
-  instance_type     = local.instance.instance_type
-  shutdown_behavior = local.instance.shutdown_behaviors[1]
-  tags              = local.instance.instance_tags
+  source                  = "../modules/ec2_instance"
+  ami                     = data.aws_ami.amazon_linux_2.id
+  instance_type           = local.instance.instance_type
+  shutdown_behavior       = local.instance.shutdown_behaviors[1]
+  disable_api_termination = false
+  tags                    = local.instance.instance_tags
 
 }
