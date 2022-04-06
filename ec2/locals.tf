34e4e177d654d9a95d428b810349209e9573ef0c
Jose Barrantes <jose.barrantes@hotmail.es>
2022-04-05 21:33:53 -0500

    Refactor local values

--- ec2/locals.tf
+++ ec2/locals.tf
@@ -2,16 +2,22 @@ locals {
   # aws region
   region = "us-east-1"
 
-  # image owner alias
-  ami_owner_alias = "amazon"
+  #instance
+  instance = {
+    # image owner alias
+    ami_owner_alias = "amazon"
 
-  # instance type 
-  instance_type = "t2.micro"
+    # instance type 
+    instance_type = "t2.micro"
 
-  # instance tags
-  instance_tags = {
-    Name      = "Jose-EC2-Demo"
-    Creator   = "JoseB"
-    ManagedBy = "Terraform"
+    # shutdown behaviors
+    shutdown_behaviors = ["stop", "terminate"]
+
+    # instance tags
+    instance_tags = {
+      Name      = "Jose-EC2-Demo"
+      Creator   = "JoseB"
+      ManagedBy = "Terraform"
+    }
   }
 }
