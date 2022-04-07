d857ad693e7e7f73569e4c9067e3e4d8d969805b
Jose Barrantes <jose.barrantes@hotmail.es>
2022-04-07 01:15:22 -0500

    Output sg id

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -9,3 +9,7 @@ output "instance_id" {
 output "instance_public_ip" {
   value = module.ec2_instance.instance_public_ip
 }
+
+output "security_group_id" {
+  value = module.security_group.security_group_id
+}
