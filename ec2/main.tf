a47d3f2ef0b8080326bddb72f0f0db05a040c7a8
Jose Barrantes <jose.barrantes@hotmail.es>
2022-04-07 01:15:35 -0500

    sg creation

--- ec2/main.tf
+++ ec2/main.tf
@@ -7,12 +7,22 @@ resource "aws_key_pair" "key_pair_demo" {
   }
 }
 
+# security group for the instance
+module "security_group" {
+  source = "../modules/security_group"
+  name   = local.security_group.name
+  vpc_id = data.aws_vpc.default_vpc.id
+  tags = {
+    Name = local.security_group.name
+  }
+}
+
 # instance creation
 module "ec2_instance" {
   source                  = "../modules/ec2_instance"
   ami                     = data.aws_ami.amazon_linux_2.id
   instance_type           = local.instance.instance_type
-  shutdown_behavior       = local.instance.shutdown_behaviors[1]
+  shutdown_behavior       = local.instance.shutdown_behaviors[0]
   disable_api_termination = false
   key_name                = aws_key_pair.key_pair_demo.id
   tags = {
